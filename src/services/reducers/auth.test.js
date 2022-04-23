fbcc5cc54ab2eb9d6db08908d3e90b7a8c8e1e58
ndreij <ndreij@gmail.com>
2022-04-23 23:54:17 +0200

    add feedback

--- src/services/reducers/auth.test.js
+++ src/services/reducers/auth.test.js
@@ -1,453 +1,282 @@
 import { authReducer } from './auth'
 import * as types from '../constants'
 
+const initialState = {
+  isRegistering: false,
+  registrationFailed: false,
+  accessToken: '',
+  refreshToken: '',
+  isLoggingIn: false,
+  isLoggedIn: false,
+  logInFailed: false,
+  isAuthLoaded: false,
+  logOutFailed: false,
+  userEmail: '',
+  userName: '',
+  isForgotPasswordEmailSent: false,
+  isPasswordReset: false,
+}
+
 describe('auth reducer', () => {
-    it('should return the initial state', () => {
-      expect(authReducer(undefined, {})).toEqual(
-        {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-        }
+  it('should return the initial state', () => {
+    expect(authReducer(undefined, {})).toEqual(
+      {
+        ...initialState
+      }
+    )
+  })
+
+  it('should handle REGISTER', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.REGISTER,
+      }
       )
-    })
+    ).toEqual(
+      {
+        ...initialState,
+        isRegistering: true,
+        registrationFailed: false,
+      }
+    )
+  })
 
-    it('should handle REGISTER', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.REGISTER,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: true,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle REGISTER_SUCCESS', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.REGISTER_SUCCESS,
+        accessToken: 'token'
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isRegistering: false,
+        accessToken: 'token'
+      }
+    )
+  })
 
-      it('should handle REGISTER_SUCCESS', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.REGISTER_SUCCESS,
-            accessToken: 'token'
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: 'token',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle LOGIN', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.LOGIN,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isLoggingIn: true,
+        logInFailed: false,
+      }
+    )
+  })
 
-      it('should handle LOGIN', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.LOGIN,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggedIn: false,
-            isLoggingIn: true,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle LOGIN_SUCCESS', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.LOGIN_SUCCESS,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isLoggingIn: false,
+        isLoggedIn: true,
+        logInFailed: false,
+        isAuthLoaded: true,
+      }
+    )
+  })
 
-      it('should handle LOGIN_SUCCESS', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.LOGIN_SUCCESS,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: true,
-            logInFailed: false,
-            isAuthLoaded: true,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle LOGIN_FAILED', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.LOGIN_FAILED,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isLoggingIn: false,
+        logInFailed: true,
+        isAuthLoaded: true,
+      }
+    )
+  })
 
-      it('should handle LOGIN_FAILED', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.LOGIN_FAILED,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',  
-            isLoggedIn: false,
-            isLoggingIn: false,
-            logInFailed: true,
-            isAuthLoaded: true,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle LOGOUT_SUCCESS', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.LOGOUT_SUCCESS,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isLoggingIn: false,
+      }
+    )
+  })
 
-      it('should handle LOGOUT_SUCCESS', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.LOGOUT_SUCCESS,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle LOGOUT_FAILED', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.LOGOUT_FAILED,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        logOutFailed: true
+      }
+    )
+  })
 
-      it('should handle LOGOUT_FAILED', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.LOGOUT_FAILED,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: true,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle AUTH', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.AUTH,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isLoggedIn: false,
+        isLoggingIn: true
+      }
+    )
+  })
 
-      it('should handle AUTH', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.AUTH,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: true,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle AUTH_SUCCESS', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.AUTH_SUCCESS,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isLoggingIn: false,
+        isLoggedIn: true,
+        isAuthLoaded: true,
+      }
+    )
+  })
 
-      it('should handle AUTH_SUCCESS', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.AUTH_SUCCESS,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: true,
-            isAuthLoaded: true,
-            logInFailed: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle AUTH_FAILED', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.AUTH_FAILED,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isLoggingIn: false,
+        logInFailed: true,
+        isAuthLoaded: true
+      }
+    )
+  })
 
-      it('should handle AUTH_FAILED', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.AUTH_FAILED,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggedIn: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-            isLoggingIn: false,
-            logInFailed: true,
-            isAuthLoaded: true
-          }
-        )
-      })
+  it('should handle FORGOT_PASSWORD', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.FORGOT_PASSWORD,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isForgotPasswordEmailSent: false
+      }
+    )
+  })
 
-      it('should handle FORGOT_PASSWORD', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.FORGOT_PASSWORD,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle FORGOT_PASSWORD_SUCCESS', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.FORGOT_PASSWORD_SUCCESS,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isForgotPasswordEmailSent: true
+      }
+    )
+  })
 
-      it('should handle FORGOT_PASSWORD_SUCCESS', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.FORGOT_PASSWORD_SUCCESS,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: true,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle FORGOT_PASSWORD_FAILED', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.FORGOT_PASSWORD_FAILED,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState
+      }
+    )
+  })
 
-      it('should handle FORGOT_PASSWORD_SUCCESS', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.FORGOT_PASSWORD_SUCCESS,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: true,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle RESET_PASSWORD', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.RESET_PASSWORD,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isPasswordReset: false
+      }
+    )
+  })
 
-      it('should handle RESET_PASSWORD', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.RESET_PASSWORD,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle RESET_PASSWORD_SUCCESS', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.RESET_PASSWORD_SUCCESS,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        isPasswordReset: true
+      }
+    )
+  })
 
-      it('should handle RESET_PASSWORD_SUCCESS', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.RESET_PASSWORD_SUCCESS,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: true,
-          }
-        )
-      })
+  it('should handle RESET_PASSWORD_FAILED', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.RESET_PASSWORD_FAILED,
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+      }
+    )
+  })
 
-      it('should handle RESET_PASSWORD_FAILED', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.RESET_PASSWORD_FAILED,
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: '',
-            userName: '',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
-
-      it('should handle SET_USER_DATA', () => {
-        expect(
-            authReducer(undefined, {
-            type: types.SET_USER_DATA,
-            name: 'name',
-            email: 'email'
-          }
-        )
-        ).toEqual(
-          {
-            isRegistering: false,
-            registrationFailed: false,
-            accessToken: '',
-            refreshToken: '',
-            isLoggingIn: false,
-            isLoggedIn: false,
-            logInFailed: false,
-            isAuthLoaded: false,
-            logOutFailed: false,
-            userEmail: 'email',
-            userName: 'name',
-            isForgotPasswordEmailSent: false,
-            isPasswordReset: false,
-          }
-        )
-      })
+  it('should handle SET_USER_DATA', () => {
+    expect(
+      authReducer(undefined, {
+        type: types.SET_USER_DATA,
+        name: 'name',
+        email: 'email'
+      }
+      )
+    ).toEqual(
+      {
+        ...initialState,
+        userEmail: 'email',
+        userName: 'name',
+      }
+    )
+  })
 
 })
